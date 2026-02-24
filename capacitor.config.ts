9d42540e4fe00caa3df126bb1eb21d19d9464126
Your Name <your.email@example.com>
2026-02-24 15:18:44 -0800

    fix: allow http navigation and set androidScheme to http to fix mixed content

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,13 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.smartmanage.app',
   appName: 'SmartManage',
-  webDir: 'out'
+  webDir: 'out',
+  server: {
+    androidScheme: 'http',
+    allowNavigation: [
+      '192.168.0.28:4000'
+    ]
+  }
 };
 
 export default config;
